403db2f791febe7accc3d9f5ec86464b0b8ba971
varun <varun80042@gmail.com>
2024-07-23 00:53:38 +0530

    update retrieval

--- spark-solution.py
+++ spark-solution.py
@@ -1,41 +1,33 @@
 import os
 import sys
+import torch
 from pyspark import SparkContext
 from pyspark.streaming import StreamingContext
-import torch
 
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '.')))
-
-from tools.data_utils import read_glove_vecs, sentences_to_indices
 from tools.model import NN, pretrained_embedding_layer
 from tools.predict import predict
+from tools.get_model import getModel
 
-glove_file = './data/glove.6B.200d.txt'
-model_file = './checkpoint/model.pth'
-word_to_index, index_to_word, word_to_vec_map = read_glove_vecs(glove_file)
-maxLen = 10  
+LOCAL_MODEL_PATH = './checkpoint/model.pth'
+MAX_LEN = 20  # max number of words
 
-model = torch.load(model_file)
+if not os.path.exists(LOCAL_MODEL_PATH):
+    print("No checkpoint found!")
+    getModel()
+
+model = torch.load(LOCAL_MODEL_PATH)
 model.eval()
-
-labels_dict = {
-    0 : "Loving",
-    1 : "Playful",
-    2 : "Happy",
-    3 : "Annoyed",
-    4 : "Foodie",
-}
 
 def process_rdd(rdd):
     if not rdd.isEmpty():
         sentences = rdd.collect()
         for sentence in sentences:
-            predict(model, sentence, word_to_index, maxLen, labels_dict)
+            predict(model, sentence, MAX_LEN)
 
 if __name__ == "__main__":
     sc = SparkContext(appName="SparkMotion")
     ssc = StreamingContext(sc, 10)
-    ssc.sparkContext.setLogLevel("WARN")
+    ssc.sparkContext.setLogLevel("ERROR")
 
     lines = ssc.socketTextStream("localhost", 9999)
     lines.foreachRDD(process_rdd)
